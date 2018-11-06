099afe4bcbc29b56f79ac1d5acdc2c0574e9442d
yakup <yakup.cetinkaya@gmail.com>
2018-11-07 00:34:41 +0300

    boot.rb

--- lib/boot.sh
+++ lib/boot.sh
@@ -1,12 +1,9 @@
 #! /bin/bash
 /usr/bin/echo `fortune -s` > /var/www/gauge/public/data/fortune.txt
-[[ -s "/home/ro/.rvm/scripts/rvm" ]] && . "/home/ro/.rvm/scripts/rvm"
-source /home/ro/.rvm/scripts/rvm
-rvm use 2.4.1@gauge
 touch /home/ro/bootscript.working
 killall ruby
-/home/ro/.rvm/rubies/ruby-2.4.1/bin/ruby /var/www/gauge/lib/socket.rb &
-/home/ro/.rvm/gems/ruby-2.4.1@gauge/bin/pumactl -F /var/www/gauge/lib/puma.rb stop  > /dev/null 2>&1
+/home/ro1/.rbenv/shims/ruby /var/www/gauge/lib/socket.rb &
+~/.rbenv/shims/pumactl -F /var/www/gauge/lib/puma.rb stop  > /dev/null 2>&1
 sleep 1
-/home/ro/.rvm/gems/ruby-2.4.1@gauge/bin/pumactl -F /var/www/gauge/lib/puma.rb start  > /dev/null 2>&1
+~/.rbenv/shims/pumactl -F /var/www/gauge/lib/puma.rb start  > /dev/null 2>&1
 
