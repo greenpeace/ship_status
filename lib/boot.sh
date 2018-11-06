4989eb7bb20e183b3d8d0109c1a407c151319c87
yakup <yakup.cetinkaya@gmail.com>
2018-11-07 01:11:56 +0300

    boot fix

--- lib/boot.sh
+++ lib/boot.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 /usr/bin/echo `fortune -s` > /var/www/gauge/public/data/fortune.txt
-touch /home/ro/bootscript.working
+touch /home/ro1/bootscript.working
 killall ruby
 /home/ro1/.rbenv/shims/ruby /var/www/gauge/lib/socket.rb &
-~/.rbenv/shims/pumactl -F /var/www/gauge/lib/puma.rb stop  > /dev/null 2>&1
+/home/ro1/.rbenv/shims/pumactl -F /var/www/gauge/lib/puma.rb stop  > /dev/null 2>&1
 sleep 1
-~/.rbenv/shims/pumactl -F /var/www/gauge/lib/puma.rb start  > /dev/null 2>&1
+/home/ro1/.rbenv/shims/pumactl -F /var/www/gauge/lib/puma.rb start  > /dev/null 2>&1
 
