0012b28bfd069947f19d687c22935bc0b10f84eb
yakup <yakup.cetinkaya@gmail.com>
2018-11-22 11:38:50 +0100

    playback at boot

--- lib/boot.sh
+++ lib/boot.sh
@@ -3,6 +3,6 @@
 touch /home/ro1/bootscript.working
 killall ruby
 /home/ro1/.rbenv/shims/ruby /var/www/gauge/lib/socket.rb &
-/home/ro1/.rbenv/shims/pumactl -F /var/www/gauge/lib/puma.rb phased-restart  > /dev/null 2>&1
-/home/ro1/.rbenv/shims/pumactl -F /var/www/playback/lib/puma.rb phased-restart  > /dev/null 2>&1
+/home/ro1/.rbenv/shims/pumactl -F /var/www/gauge/lib/puma.rb phased_restart  > /dev/null 2>&1
+/home/ro1/.rbenv/shims/pumactl -F /var/www/playback/lib/puma.rb phased_restart  > /dev/null 2>&1
 
