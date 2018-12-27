aac418dd9182d62f9639ccaadad6ad9da70855f3
yakup <yakup.cetinkaya@gmail.com>
2018-12-27 11:55:08 +0100

    boot

--- lib/boot.sh
+++ lib/boot.sh
@@ -4,8 +4,10 @@ touch /home/ro1/bootscript.working
 killall ruby
 /home/ro1/.rbenv/shims/ruby /var/www/gauge/lib/socket.rb &
 /home/ro1/.rbenv/shims/pumactl -F /var/www/gauge/lib/puma.rb stop  > /dev/null 2>&1
+/home/ro1/.rbenv/shims/pumactl -F /var/www/erlog/lib/puma.rb stop  > /dev/null 2>&1
 /home/ro1/.rbenv/shims/pumactl -F /var/www/playback/lib/puma.rb stop  > /dev/null 2>&1
 sleep 1
 /home/ro1/.rbenv/shims/pumactl -F /var/www/gauge/lib/puma.rb start  > /dev/null 2>&1
+/home/ro1/.rbenv/shims/pumactl -F /var/www/erlog/lib/puma.rb start  > /dev/null 2>&1
 /home/ro1/.rbenv/shims/pumactl -F /var/www/playback/lib/puma.rb start  > /dev/null 2>&1
 
